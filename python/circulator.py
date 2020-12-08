1e3c7dfa5561216326ced54451b07a5fca0ab491
Brian Seel <Brian.Seel@baltimorecity.gov>
2020-12-08 14:39:09 -0500

    Making the stop a merge condition as well

--- python/circulator.py
+++ python/circulator.py
@@ -27,6 +27,7 @@ logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
 
 conn = pyodbc.connect(r'Driver={SQL Server};Server=balt-sql311-prd;Database=DOT_DATA;Trusted_Connection=yes;')
+cursor = conn.cursor()
 
 
 def update_database(start_date, end_date):
@@ -34,36 +35,39 @@ def update_database(start_date, end_date):
     logging.info("Processing %s to %s", start_date.strftime('%m/%d/%y'), end_date.strftime('%m/%d/%y'))
     rs_cls = Reports(RIDESYSTEMS_USERNAME, RIDESYSTEMS_PASSWORD)
 
+    data = []
     for search_date in daterange(start_date, end_date):
         for row in rs_cls.get_otp(search_date, search_date):
-            conn.execute("""
-            MERGE [ccc_arrival_times2] USING (
-            VALUES
-                (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-            ) AS vals (date, route, stop, blockid, scheduledarrivaltime, actualarrivaltime, scheduleddeparturetime,
-            actualdeparturetime, ontimestatus, vehicle)
-            ON (ccc_arrival_times2.date = vals.date AND
-                ccc_arrival_times2.route = vals.route AND
-                ccc_arrival_times2.blockid = vals.blockid)
-            WHEN MATCHED THEN
-                UPDATE SET
-                scheduledarrivaltime = vals.scheduledarrivaltime,
-                actualarrivaltime = vals.actualarrivaltime,
-                scheduleddeparturetime = vals.scheduleddeparturetime,
-                actualdeparturetime = vals.actualdeparturetime,
-                stop = vals.stop,
-                ontimestatus = vals.ontimestatus,
-                vehicle = vals.vehicle
-            WHEN NOT MATCHED THEN
-                INSERT (date, route, stop, blockid, scheduledarrivaltime, actualarrivaltime, scheduleddeparturetime,
-                    actualdeparturetime, ontimestatus, vehicle)
-                VALUES (vals.date, vals.route, vals.stop, vals.blockid, vals.scheduledarrivaltime,
-                    vals.actualarrivaltime, vals.scheduleddeparturetime, vals.actualdeparturetime, vals.ontimestatus,
-                    vals.vehicle);
-            """, row['date'], row['route'], row['stop'], row['blockid'], row['scheduledarrivaltime'],
+            data.append((row['date'], row['route'], row['stop'], row['blockid'], row['scheduledarrivaltime'],
                          row['actualarrivaltime'], row['scheduleddeparturetime'], row['actualdeparturetime'],
-                         row['ontimestatus'], row['vehicle'])
-            conn.commit()
+                         row['ontimestatus'], row['vehicle']))
+
+    cursor.executemany("""
+        MERGE [ccc_arrival_times2] USING (
+        VALUES
+            (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ) AS vals (date, route, stop, blockid, scheduledarrivaltime, actualarrivaltime, scheduleddeparturetime,
+        actualdeparturetime, ontimestatus, vehicle)
+        ON (ccc_arrival_times2.date = vals.date AND
+            ccc_arrival_times2.route = vals.route AND
+            ccc_arrival_times2.blockid = vals.blockid AND
+            ccc_arrival_times2.stop = vals.stop)
+        WHEN MATCHED THEN
+            UPDATE SET
+            scheduledarrivaltime = vals.scheduledarrivaltime,
+            actualarrivaltime = vals.actualarrivaltime,
+            scheduleddeparturetime = vals.scheduleddeparturetime,
+            actualdeparturetime = vals.actualdeparturetime,
+            ontimestatus = vals.ontimestatus,
+            vehicle = vals.vehicle
+        WHEN NOT MATCHED THEN
+            INSERT (date, route, stop, blockid, scheduledarrivaltime, actualarrivaltime, scheduleddeparturetime,
+                actualdeparturetime, ontimestatus, vehicle)
+            VALUES (vals.date, vals.route, vals.stop, vals.blockid, vals.scheduledarrivaltime,
+                vals.actualarrivaltime, vals.scheduleddeparturetime, vals.actualdeparturetime, vals.ontimestatus,
+                vals.vehicle);
+    """, data)
+    conn.commit()
 
 
 def daterange(start_date, end_date):
