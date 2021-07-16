a22cc530fd6ba166e8870bb2ed965b709c56dbad
Brian Seel <Brian.Seel@baltimorecity.gov>
2021-07-16 16:51:35 -0400

    Add creds

--- src/transitstat/circulator/reports.py
+++ src/transitstat/circulator/reports.py
@@ -6,6 +6,7 @@ from loguru import logger
 from ridesystems.reports import Reports
 from sqlalchemy import create_engine  # type: ignore
 from sqlalchemy.orm import Session  # type: ignore
+from typing import Optional
 
 from .creds import RIDESYSTEMS_USERNAME, RIDESYSTEMS_PASSWORD
 from .schema import CirculatorArrival
@@ -13,7 +14,7 @@ from .._merge import insert_or_update
 
 
 def get_otp(start_date: date, end_date: date, conn_str: str,  # pylint:disable=too-many-locals
-            force: bool = False) -> None:
+            force: bool = False, rs_user: Optional[str] = None, rs_pass: Optional[str] = None) -> None:
     """
     Gets the data from the ride systems scraper and puts it in the database
 
@@ -21,9 +22,16 @@ def get_otp(start_date: date, end_date: date, conn_str: str,  # pylint:disable=t
     :param end_date: Last date (inclusive) to write to the database
     :param conn_str: Database connection string
     :param force: Regenerate the data for the date range. By default, it skips dates with existing data.
+    :param rs_user: Ridesystems username
+    :param rs_pass: Ridesystems password
     """
     logger.info("Processing bus arrivals: {} to {}", start_date.strftime('%m/%d/%y'), end_date.strftime('%m/%d/%y'))
-    rs_cls = Reports(RIDESYSTEMS_USERNAME, RIDESYSTEMS_PASSWORD)
+    if rs_user is None:
+        rs_user = RIDESYSTEMS_USERNAME
+    if rs_pass is None:
+        rs_pass = RIDESYSTEMS_PASSWORD
+
+    rs_cls = Reports(rs_user, rs_pass)
 
     engine = create_engine(conn_str, echo=True, future=True)
 
