94f2132d8905b5ac76b05d7bc26834b85a3d2e56
quilvioo <quilvio.a.hernandez@gmail.com>
2022-04-22 12:06:19 -0400

    updating data_import to handle new format

--- tests/test_connector_import.py
+++ tests/test_connector_import.py
@@ -12,15 +12,15 @@ from transitstat.connector.schema import HcRidership
 def test_parse_sheets(tmp_path_factory, connector_import):
     """Test for parse_sheets with a local file"""
     # expected values
-    exp_vals = {date(2021, 8, 2): 135, date(2021, 8, 3): 274, date(2021, 8, 4): 250}
+    exp_vals = {date(2022, 3, 2): 69, date(2022, 3, 3): 55, date(2022, 3, 4): 34}
 
     tmp_path = tmp_path_factory.mktemp('data')
-    shutil.copy(Path(__file__).parent / 'data' / 'HC3 08-2021.xlsx', tmp_path)
+    shutil.copy(Path(__file__).parent / 'data' / 'HC2 03-2022.xlsx', tmp_path)
     sheets = connector_import.parse_sheets(tmp_path)
-    assert len(sheets.keys()) == 1  # should only have route 3
-    assert len(sheets[3].keys()) == 3  # three dates
+    assert len(sheets.keys()) == 1  # should only have route 2
+    assert len(sheets[2].keys()) == 3  # three dates
     for key, val in exp_vals.items():
-        assert sheets[3][key] == val
+        assert sheets[2][key] == val
 
     connector_import.insert_into_db(sheets)
 
